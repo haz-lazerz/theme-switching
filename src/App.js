36553b8bbe9a1ce50270797a3e951b8856607aa1
haz-lazerz <jokubas.kantminas@gmail.com>
2019-04-17 11:58:02 +0300

    simple project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
+import { Switch } from "@material-ui/core";
 
 class App extends Component {
+  state = {
+    checked: false
+  };
+
+  handleChange = () => {
+    this.testFunc();
+    this.setState({ checked: !this.state.checked });
+  };
+
+  testFunc = () => {
+    document.documentElement.classList.add("transition");
+    window.setTimeout(() => {
+      document.documentElement.classList.remove("transition");
+    }, 1000);
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div
+        className={
+          !this.state.checked
+            ? "container-wrapper-light"
+            : "container-wrapper-dark"
+        }
+      >
+        <div className="container">
+          <h1>Light / Dark mode</h1>
+          <div className="toggler-container">
+            <Switch onChange={this.handleChange} className="custom" />
+          </div>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Ex do proident sit et tempor proident aliquip fugiat aliquip sint
+            quis. Voluptate occaecat ut adipisicing esse laborum veniam
+            occaecat. Cupidatat eu ea dolor voluptate fugiat minim sit deserunt
+            reprehenderit duis duis voluptate cillum. Pariatur consequat
+            reprehenderit adipisicing Lorem cillum enim voluptate ipsum
+            cupidatat fugiat fugiat ut pariatur eu. Excepteur anim commodo in
+            Lorem et cillum cillum culpa ad. Occaecat labore cillum et laborum
+            amet culpa. Amet eu aute veniam commodo voluptate nostrud magna
+            dolore.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        </div>
       </div>
     );
   }
